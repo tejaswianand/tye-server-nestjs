a11cee1a7f869e5f9d5b851bba90ebbedb7cc16c
Tejaswi Anand <tejaswianand85@gmail.com>
2025-01-04 18:51:04 +0530

    cors allow all

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    allowedHeaders: "*",
+    origin: "*"
   });
   await app.listen(process.env.PORT ?? 3000);
 }
